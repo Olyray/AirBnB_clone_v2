9e72948f956cb6ed4bcffbb70477b5539f41d89c
Olamide <olire96@gmail.com>
2023-04-30 12:26:49 +0100

    documentation wahala

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,27 +3,27 @@
     listens to 0.0.0.0:5000
     
 """
-from flask import Flask, render_template, url_for
-import os
 from models import storage
-from models.state import State
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Remove the current SQLAlchemy Session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Displays a list of all State objects present in DBStorage"""
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
