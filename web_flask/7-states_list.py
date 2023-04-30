4db0bb1a5cd9bb53ef39262590fb2d885c05d153
Olamide <olire96@gmail.com>
2023-04-30 12:22:17 +0100

    checking

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines a Flask web application"""
+
+from flask import Flask, render_template, url_for
+import os
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays a list of all State objects present in DBStorage"""
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
