c357eeac1bc00d0d6ced1f4162a26c1fdd577e9b
Olamide <olire96@gmail.com>
2023-05-17 18:00:51 +0000

    update

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,5 +44,10 @@ def odd_or_even(n):
     return render_template("6-number_odd_or_even.html", n=n)
 
 
+@app.route("/airbnb-dynamic/number_odd_or_even/<int:n>", strict_slashes=False)
+def airbnb_odd_or_even(n):
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
